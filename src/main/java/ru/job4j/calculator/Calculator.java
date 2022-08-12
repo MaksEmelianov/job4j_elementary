75b6b955320652b773f670f470ed01a2e8e33379
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-08-12 18:44:05 +0500

    3. Арифметические операции. [#185] - убрал комментарии

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -5,8 +5,8 @@ public class Calculator {
     public static void main(String[] args) {
         int one = 1;
         int two = 2;
-//        int onePlusTwo = one + two;
-//        System.out.println(onePlusTwo);
+        int onePlusTwo = one + two;
+        System.out.println(onePlusTwo);
 
         int four = 4;
         int five = 5;
