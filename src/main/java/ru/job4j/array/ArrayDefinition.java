1cb57272b0369ab9fc07a285b21d8a0750162460
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-08-28 18:35:05 +0500

    6.0.3. Заполнение массива. [#226300] - without for

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -16,8 +16,9 @@ public class ArrayDefinition {
         names[1] = "Petr";
         names[2] = "Slava";
         names[3] = "Misha";
-        for (int i = 0; i < names.length; i++) {
-            System.out.println(names[i]);
-        }
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
