ec6aaf1aaee56685ef44b3a0ede089c7c9a955fe
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-08-31 08:08:28 +0500

    6.8. Дефрагментация массива [#225 #337831]

--- src/main/java/ru/job4j/array/Defragmentation.java
+++ src/main/java/ru/job4j/array/Defragmentation.java
@@ -3,14 +3,12 @@ package ru.job4j.array;
 public class Defragmentation {
 
     public static String[] compress(String[] array) {
-        String tmp;
         for (int i = 0; i < array.length; i++) {
             if (array[i] == null) {
                 for (int j = i + 1; j < array.length; j++) {
                     if (array[j] != null) {
-                        tmp = array[j];
-                        array[j] = array[i];
-                        array[i] = tmp;
+                        array[i] = array[j];
+                        array[j] = null;
                         break;
                     }
                 }
